0ea1a1c2e906833fd9be9b3cb35f05c86ad2db30
alice-carre <91736662+alice-carre@users.noreply.github.com>
2021-12-10 18:38:31 +0100

    Update write_spiral_Bernstein.py

--- write_spiral_Bernstein.py
+++ write_spiral_Bernstein.py
@@ -15,9 +15,13 @@ Nx = 256 # Define resolution
 Ny = Nx
 slice_thickness = 3e-3  # Slice thickness
 N_slices = 1
-oversampling = 2
 phi = np.pi / 2
-N_shot = 3
+N_shot = 2
+delta = 2 * np.pi/N_shot
+
+# Golden Angle Case:
+#delta = 2*np.pi - (2*np.pi) * (2/(1+np.sqrt(5))) # angular increment  # angle = 137.51°
+
 
 # Set the system limits
 system = pp.Opts(max_grad=30, grad_unit='mT/m', max_slew=128, slew_unit='T/m/s', rf_ringdown_time=30e-6,
@@ -49,61 +53,46 @@ if Ts>T_acq:
 
 
 # Definition of a desired number of samples - copied on the matlab code # not sure if appropriate
-#k_radius = int(np.round(Nx / 2))
-#k_samples2=int(np.round(np.pi * k_radius * oversampling * k_radius / N_shot))
 k_samples = round(T_acq/system.grad_raster_time)
 
 t = np.linspace(0, T_acq, k_samples)
-List_grad_and_slew_rate_original = []
 
-for index in range(N_shot):
-    # Calculate a raw Archimedian spiral trajectory # constant linear velocity
-    ka = np.zeros((2, k_samples))
-    for c in range(k_samples):
-        if t[c] < Ts:
-            teta = 1 / 2 * beta * t[c] ** 2 / (capital_lbd + beta / 2 / a2 * t[c] ** (4 / 3))
-        else:
-            teta = np.sqrt(teta_s ** 2 + 2 * system.max_grad / lbd * (t[c] - Ts))
-        ka[0][c] = np.real(lbd * teta * np.exp(1j * (teta + 2 * np.pi * index / N_shot)))
-        ka[1][c] = np.imag(lbd * teta * np.exp(1j * (teta + 2 * np.pi * index / N_shot)))
-    # plt.plot(ka[0], ka[1])
+ka = np.zeros((2, k_samples))
+for c in range(k_samples):
+    if t[c] < Ts:
+        teta = 1 / 2 * beta * t[c] ** 2 / (capital_lbd + beta / 2 / a2 * t[c] ** (4 / 3))
+    else:
+        teta = np.sqrt(teta_s ** 2 + 2 * system.max_grad / lbd * (t[c] - Ts))
+    ka[0][c] = np.real(lbd * teta * np.exp(1j * (teta)))
+    ka[1][c] = np.imag(lbd * teta * np.exp(1j * (teta)))
 
     # Calculate gradients and slew rates
-    ga, sa = pp.traj_to_grad(ka)
-    List_grad_and_slew_rate_original.append((ga, sa))
+ga, sa = pp.traj_to_grad(ka)
 
 # Create 90 degree slice selection pulse and gradient
 rf, gz, gzr = pp.make_sinc_pulse(flip_angle=np.pi / 2, duration=3e-3, slice_thickness=slice_thickness,
                                  apodization=0.5, time_bw_product=4, system=system, return_gz=True)
 gz_reph = pp.make_trapezoid(channel='z', area=-gz.area / 2, system=system)
-List_spiral_grad_shape = []
-List_gradients = []
-for index in range(0, N_shot):
-    List_spiral_grad_shape.append(List_grad_and_slew_rate_original[index][0])
-    spiral_grad_shape = List_spiral_grad_shape[index]
-    # Extend spiral_grad_shape by repeating the last sample
-    # This is needed to accomodate for the ADC tuning delay
-    spiral_grad_shape = np.c_[spiral_grad_shape, spiral_grad_shape[:, -1]]
 
-    # Readout grad
-    gx = make_arbitrary_grad(channel='x', waveform=spiral_grad_shape[0], delay=pp.calc_duration(gz_reph))
-    gy = make_arbitrary_grad(channel='y', waveform=spiral_grad_shape[1], delay=pp.calc_duration(gz_reph))
+spiral_grad_shape = ga
+spiral_grad_shape = np.c_[spiral_grad_shape, spiral_grad_shape[:, -1]]
 
-    # Spoilers
-    gz_spoil = pp.make_trapezoid(channel='z', area=4 * Nx * delta_k, system=system)
-    gx_spoil = pp.make_extended_trapezoid(channel='x', amplitudes=[spiral_grad_shape[0][-1], 0],
-                                          times=[0, pp.calc_duration(gz_spoil)])
-    gy_spoil = pp.make_extended_trapezoid(channel='y', amplitudes=[spiral_grad_shape[1][-1], 0],
-                                          times=[0, pp.calc_duration(gz_spoil)])
-    List_gradients.append((gx, gy, gx_spoil, gy_spoil))
+gx = make_arbitrary_grad(channel='x', waveform=spiral_grad_shape[0], delay=pp.calc_duration(gz_reph))
+gy = make_arbitrary_grad(channel='y', waveform=spiral_grad_shape[1], delay=pp.calc_duration(gz_reph))
+
+# Spoilers
+gz_spoil = pp.make_trapezoid(channel='z', area=4 * Nx * delta_k, system=system)
+gx_spoil = pp.make_extended_trapezoid(channel='x', amplitudes=[spiral_grad_shape[0][-1], 0],
+                                      times=[0, pp.calc_duration(gz_spoil)])
+gy_spoil = pp.make_extended_trapezoid(channel='y', amplitudes=[spiral_grad_shape[1][-1], 0],
+                                      times=[0, pp.calc_duration(gz_spoil)])
 
 # Calculate ADC
 #To be tested on the scanner
 adc_time = T_acq
-adc_samples = round(k_samples*oversampling)
+adc_samples = round(k_samples)
 adc_dwell = round(adc_time / adc_samples / 100e-9) * 100e-9  # on Siemens adc_dwell needs to be aligned to 100ns
 adc = pp.make_adc(num_samples=adc_samples, dwell=adc_dwell, delay=pp.calc_duration(gz_reph))
-
 
 """
 # Calculate ADC
@@ -131,10 +120,6 @@ adc = pp.make_adc(num_samples=adc_samples, dwell=adc_dwell, delay=pp.calc_durati
 for s in range(0, N_slices):
     for i in range(0, N_shot):
         # seq.add_block(rf_fs,gz_fs) # fat-sat
-        gx = List_gradients[i][0]
-        gy = List_gradients[i][1]
-        gx_spoil = List_gradients[i][2]
-        gy_spoil = List_gradients[i][3]
         rf.freq_offset = gz.amplitude * slice_thickness * (s - (N_slices - 1) / 2)
         seq.add_block(rf, gz)
 
@@ -142,7 +127,7 @@ for s in range(0, N_slices):
         # Not very clean way to add events but the best way I found since add_block wouldn't take a list[SimpleNameSpace] as argument
         # The list returned by the rotate function has a variable length since sometimes gradients have to be projected on one or two axis
 
-        rot1 = rotate('z', phi, gz_reph, gx, gy, adc)
+        rot1 = rotate('z', i*delta, gz_reph, gx, gy, adc)
         if len(rot1) == 1:
             seq.add_block(rot1[0])
         elif len(rot1) == 2:
@@ -156,7 +141,7 @@ for s in range(0, N_slices):
         else:
             raise TypeError("number of rotated inputs not supported")
 
-        rot2 = rotate('z', phi, gx_spoil, gy_spoil, gz_spoil)
+        rot2 = rotate('z', i*delta, gx_spoil, gy_spoil, gz_spoil)
         if len(rot2) == 1:
             seq.add_block(rot2[0])
         elif len(rot2) == 2:
