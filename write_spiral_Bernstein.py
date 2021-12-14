90f1c77b3c6e736a1b4130d57dc924b6670470ee
alice-carre <91736662+alice-carre@users.noreply.github.com>
2021-12-14 12:46:25 +0100

    Update write_spiral_Bernstein.py

--- write_spiral_Bernstein.py
+++ write_spiral_Bernstein.py
@@ -15,7 +15,7 @@ Nx = 256 # Define resolution
 Ny = Nx
 slice_thickness = 3e-3  # Slice thickness
 N_slices = 1
-phi = np.pi / 2
+phi = 10
 N_shot = 2
 delta = 2 * np.pi/N_shot
 
@@ -24,12 +24,8 @@ delta = 2 * np.pi/N_shot
 
 
 # Set the system limits
-system = pp.Opts(max_grad=30, grad_unit='mT/m', max_slew=128, slew_unit='T/m/s', rf_ringdown_time=30e-6,
+system = pp.Opts(max_grad=25, grad_unit='mT/m', max_slew=100, slew_unit='T/m/s', rf_ringdown_time=30e-6,
                  rf_dead_time=100e-6, adc_dead_time=10e-6)
-
-# Create 90 degree slice selection pulse and gradient
-rf, gz, gzr = pp.make_sinc_pulse(flip_angle=phi, duration=3e-3, slice_thickness=slice_thickness,
-                                 apodization=0.5, time_bw_product=4, system=system, return_gz=True)
 
 # Define k-space parameters
 delta_k = 1 / fov
@@ -51,8 +47,7 @@ T_acq = Ts + lbd / (system.max_grad * 2) * (teta_max ** 2 - teta_s ** 2)
 if Ts>T_acq:
     T_acq = 2*np.pi*fov/(3*N_shot)*np.sqrt(1/(2*system.max_slew*(delta_x)**3)) # for large Go, slow SR0, small Nshot, or large FOV
 
-
-# Definition of a desired number of samples - copied on the matlab code # not sure if appropriate
+# Definition of a desired number of samples 
 k_samples = round(T_acq/system.grad_raster_time)
 
 t = np.linspace(0, T_acq, k_samples)
@@ -69,9 +64,10 @@ for c in range(k_samples):
     # Calculate gradients and slew rates
 ga, sa = pp.traj_to_grad(ka)
 
-# Create 90 degree slice selection pulse and gradient
-rf, gz, gzr = pp.make_sinc_pulse(flip_angle=np.pi / 2, duration=3e-3, slice_thickness=slice_thickness,
+# Create slice selection pulse and gradient
+rf, gz, gzr = pp.make_sinc_pulse(flip_angle=phi*np.pi/180, duration=3e-3, slice_thickness=slice_thickness,
                                  apodization=0.5, time_bw_product=4, system=system, return_gz=True)
+
 gz_reph = pp.make_trapezoid(channel='z', area=-gz.area / 2, system=system)
 
 spiral_grad_shape = ga
@@ -89,37 +85,14 @@ gy_spoil = pp.make_extended_trapezoid(channel='y', amplitudes=[spiral_grad_shape
 
 # Calculate ADC
 #To be tested on the scanner
-adc_time = T_acq
+adc_time = k_samples*system.grad_raster_time
 adc_samples = round(k_samples)
 adc_dwell = round(adc_time / adc_samples / 100e-9) * 100e-9  # on Siemens adc_dwell needs to be aligned to 100ns
 adc = pp.make_adc(num_samples=adc_samples, dwell=adc_dwell, delay=pp.calc_duration(gz_reph))
 
-"""
-# Calculate ADC
-adc_time = np.shape(spiral_grad_shape)[1]*system.grad_raster_time
-# the (Siemens) interpreter sequence
-# per default will try to split the trajectory into segments <=1000 samples
-# and every of these segments will have to have duration aligned to the
-# gradient raster time
-adc_samples_per_segment = 1000  # may be needed to play with this number to fill the entire trajectory
-adc_samples_desired = k_samples
-adc_segments = round(adc_samples_desired / adc_samples_per_segment)
-adc_samples = adc_segments * adc_samples_per_segment
-adc_dwell = round(adc_time / adc_samples / 100e-9) * 100e-9  # on Siemens adc_dwell needs to be aligned to 100ns
-
-adc_segment_duration = adc_samples_per_segment * adc_dwell
-if np.floor(divmod(adc_segment_duration, system.grad_raster_time)[1]) > np.finfo(float).eps:
-    raise TypeError("ADC segmentation model results in incorrect segment duration")
-# Update segment count
-adc_segments = np.floor(adc_time / adc_segment_duration)
-adc_samples = adc_segments * adc_samples_per_segment
-adc = pp.make_adc(num_samples=adc_samples, dwell=adc_dwell, delay=pp.calc_duration(gz_reph))
-"""
-
 # Define sequence blocks
 for s in range(0, N_slices):
     for i in range(0, N_shot):
-        # seq.add_block(rf_fs,gz_fs) # fat-sat
         rf.freq_offset = gz.amplitude * slice_thickness * (s - (N_slices - 1) / 2)
         seq.add_block(rf, gz)
 
@@ -166,7 +139,7 @@ else:
 write_seq_definitions(seq, fov=fov, slice_thickness=slice_thickness, Name='spiral', alpha = phi, Nx=Nx,
                       Sampling_scheme='spiral', Ny=Ny, N_slices=N_slices, N_interleaves = N_shot)
 #seq.set_definition('MaxAdcSegmentLength', adc_samples_per_segment)
-seq.write('spiral.seq')  # Output sequence for scanner
+seq.write('Bernstein_spiral.seq')  # Output sequence for scanner
 
 plt.figure()
 seq.plot()
